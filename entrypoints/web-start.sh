2716459caed544a524a174e8b7d4904ba8b46c64
Guilherme Ferreira <guilherme.ferreira@visagio.com>
2022-07-15 23:52:24 -0300

    fix continaaer startup

--- entrypoints/web-start.sh
+++ entrypoints/web-start.sh
@@ -4,23 +4,23 @@ cd $APP_ROOT
 
 if [ "$RAILS_ENV" == "development" ]
 then
-  # # Check if postgres already start
-  # IS_READY='accepting connections'
-  # PG_READY=$(pg_isready -h $DB_HOST -U postgres 2>&1 >/dev/null && echo $IS_READY)
-  # SLEEP_TIME=5
-  # MAX_SLEEP_TIME=60
-  # SLEEP_STEP=5
-  # while [[ "$PG_READY" != "$IS_READY" ]]
-  # do
-  #   PG_READY=$(pg_isready -h $DB_HOST -U postgres 2>&1 >/dev/null && echo $IS_READY)
-  #   echo "Awaiting postgres start...";
+  # Check if postgres already start
+  IS_READY='accepting connections'
+  PG_READY=$(pg_isready -h $DB_HOST -U postgres 2>&1 >/dev/null && echo $IS_READY)
+  SLEEP_TIME=5
+  MAX_SLEEP_TIME=60
+  SLEEP_STEP=5
+  while [[ "$PG_READY" != "$IS_READY" ]]
+  do
+    PG_READY=$(pg_isready -h $DB_HOST -U postgres 2>&1 >/dev/null && echo $IS_READY)
+    echo "Awaiting postgres start...";
 
-  #   sleep $SLEEP_TIME;
-  #   if [ $SLEEP_TIME -lt $MAX_SLEEP_TIME ]
-  #   then
-  #     SLEEP_TIME=$(($SLEEP_TIME + $SLEEP_STEP))
-  #   fi
-  # done
+    sleep $SLEEP_TIME;
+    if [ $SLEEP_TIME -lt $MAX_SLEEP_TIME ]
+    then
+      SLEEP_TIME=$(($SLEEP_TIME + $SLEEP_STEP))
+    fi
+  done
 
   # Check if gems already installed
   GEMS_INSTALLED=$(bundle check)
