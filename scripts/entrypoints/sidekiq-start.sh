6d67991add7991aa114b3602543a9c7c2e1db67a
Guilherme Ferreira <guilherme.ferreira@visagio.com>
2023-04-24 19:26:34 -0300

    ble

--- scripts/entrypoints/sidekiq-start.sh
+++ scripts/entrypoints/sidekiq-start.sh
@@ -2,26 +2,26 @@
 
 cd $APP_ROOT
 
-if [ "$RAILS_ENV" == "development" ]
-then
-  # Check if gems already installed
-  GEMS_INSTALLED=$(bundle check)
-  IS_INSTALLED="The Gemfile's dependencies are satisfied"
-  SLEEP_TIME=5
-  MAX_SLEEP_TIME=60
-  SLEEP_STEP=5
-  while [[ "$GEMS_INSTALLED" != "$IS_INSTALLED" ]]
-  do
-    GEMS_INSTALLED=$(bundle check)
-    echo "Awaiting gems are installed...";
+# if [ "$RAILS_ENV" == "development" ]
+# then
+#   # Check if gems already installed
+#   GEMS_INSTALLED=$(bundle check)
+#   IS_INSTALLED="The Gemfile's dependencies are satisfied"
+#   SLEEP_TIME=5
+#   MAX_SLEEP_TIME=60
+#   SLEEP_STEP=5
+#   while [[ "$GEMS_INSTALLED" != "$IS_INSTALLED" ]]
+#   do
+#     GEMS_INSTALLED=$(bundle check)
+#     echo "Awaiting gems are installed...";
 
-    sleep $SLEEP_TIME;
-    if [ $SLEEP_TIME -lt $MAX_SLEEP_TIME ]
-    then
-      SLEEP_TIME=$(($SLEEP_TIME + $SLEEP_STEP))
-    fi
-  done
-fi
+#     sleep $SLEEP_TIME;
+#     if [ $SLEEP_TIME -lt $MAX_SLEEP_TIME ]
+#     then
+#       SLEEP_TIME=$(($SLEEP_TIME + $SLEEP_STEP))
+#     fi
+#   done
+# fi
 
 if [ -f $PIDFILE ]; then
   rm -f $PIDFILE
