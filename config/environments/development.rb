726cb87ad89a1bc324ab777f0ac04081016c217c
Piradata <piradata@duck.com>
2023-05-14 20:50:04 +0000

    add sourcemaps to debugging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  # Generate sourcemap for js files
+  config.assets.debug = true
+
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
 
