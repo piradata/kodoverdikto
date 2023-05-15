f66074b3548ee4e294a4366a1829a767f9775fce
Piradata <piradata@duck.com>
2023-05-15 03:39:00 +0000

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV.fetch("RAILS_SERVE_STATIC_FILES", false)
+  config.public_file_server.enabled = (ENV["RAILS_SERVE_STATIC_FILES"] == "true")
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sassc
@@ -34,6 +34,10 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
+  if ENV["ASSETS_HOST"].present?
+    config.action_controller.asset_host = ENV["ASSETS_HOST"]
+    config.action_mailer.asset_host = config.action_controller.asset_host
+  end
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
