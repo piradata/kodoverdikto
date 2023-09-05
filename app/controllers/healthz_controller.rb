af3530896f7514db4ca8931f726574cee9ea9310
Piradata <piradata@duck.com>
2023-09-05 00:23:28 -0300

    RENDER_GIT_COMMIT

--- app/controllers/healthz_controller.rb
+++ app/controllers/healthz_controller.rb
@@ -4,7 +4,8 @@ class HealthzController < ApplicationController
   def healthz
     render json: {
       status: 'OK',
-      commit: ENV.fetch('COMMIT_HASH', nil).to_s
+      commit: (ENV.fetch('COMMIT_HASH', nil) || ENV.fetch('RENDER_GIT_COMMIT', nil)).to_s,
+      branch: (ENV.fetch('COMMIT_HASH', nil) || ENV.fetch('RENDER_GIT_BRANCH', nil)).to_s
     }
   end
 
